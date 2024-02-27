770370d6cc8854e99909daa736dffb8c7dcf7202
kwizerao <o.kwizera@alustudent.com>
2024-02-27 18:05:41 +0200

    initial

--- csharp-hello_world/100-hello_error/100-hello_error.cs
+++ csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -1,1 +1,10 @@
-﻿Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+         // Exit with status code 1
+        Environment.Exit(1);
+    }
+}
