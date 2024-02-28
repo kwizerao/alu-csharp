c976300eb22381614a498878e1bc53d2d5621136
kwizerao <o.kwizera@alustudent.com>
2024-02-28 15:35:18 +0200

    initial

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+class Array{
+
+    public static int[] CreatePrint(int size){
+
+        if(size == 0){
+            Console.WriteLine();
+            return new int[0];
+        }else if(size < 0){
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] NumberList = new int[size];
+        for(int i = 0; i < size; i ++){
+            NumberList[i] = i;
+            if(i < size - 1){
+                Console.Write($"{i} ");
+            }else{
+                Console.Write($"{i}");
+            }
+         
+
+        }
+        Console.WriteLine();
+
+        return NumberList;
+    }
+}
