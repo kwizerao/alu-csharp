dc124da6039599c067761ba11ea791c172fa13f1
kwizerao <o.kwizera@alustudent.com>
2024-02-28 13:18:01 +0200

    initial

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 
-class Program{
-    static void Main(String[] args){
-         for(int i = 0; i < 100 ; i++){
-            
-            if(i == 99){
-                Console.Write($"{i}");
-            }else{
-                Console.Write($"{i:D2}, ");
-            }
-         }
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write($"{i:D2}" + (i < 99 ? ", " : "\n"));
         }
     }
+}
