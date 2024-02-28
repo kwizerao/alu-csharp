b628f7a1ecfb200c2cd619086c90d7d1cf901225
kwizerao <o.kwizera@alustudent.com>
2024-02-28 13:33:50 +0200

    initial

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+class Line{
+    public static void PrintLine(int length){
+        if(length > 0){
+                for(int i = 0 ; i < length; i++){
+                Console.Write("_");
+             }
+        }
+        Console.WriteLine();       
+    }
+}
