4de69c172b09b31dd65d070de6579c8454983e41
kwizerao <o.kwizera@alustudent.com>
2024-02-28 13:36:41 +0200

    initial

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Line{
+    public static void PrintDiagonal(int length){
+          if(length > 0){
+                for(int i = 0 ; i < length; i++){
+                Console.Write("\\");
+             }
+        }
+        Console.WriteLine(); 
+    }
+}
