92bd9dcb5d6da41045f38dcf42187ca32b9fd78c
kwizerao <o.kwizera@alustudent.com>
2024-02-28 17:13:17 +0200

    initial

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int count = 0;
+        foreach (var key in myDict.Keys)
+        {
+            count++;
+        }
+
+        return count;
+    }
+}
