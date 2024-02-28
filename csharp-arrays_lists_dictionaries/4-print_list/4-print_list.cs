60d37ecca48dd3a1569e0b6b211bd33a5374ab42
kwizerao <o.kwizera@alustudent.com>
2024-02-28 16:33:50 +0200

    initial

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            List<int> CacheList = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(i);
+                CacheList.Add(i);
+
+                if (i < size - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+
+            Console.WriteLine();
+            return CacheList;
+        }
+    }
+}
