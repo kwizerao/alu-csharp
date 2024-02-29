dfde5143ed974a216e680f9146caf7be4d961153
kwizerao <o.kwizera@alustudent.com>
2024-02-29 16:17:48 +0200

    initial

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+class List{
+    public static int Sum(List<int> myList){
+        HashSet<int> Elements = new HashSet<int>(myList);
+        int sum = 0;
+        foreach(int value in Elements){
+            sum += value;
+        }
+
+        return sum;
+    }
+}
