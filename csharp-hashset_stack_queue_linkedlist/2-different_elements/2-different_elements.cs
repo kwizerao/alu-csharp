e4db16ba77ad1818461a57f0770c859d1e3b12b5
kwizerao <o.kwizera@alustudent.com>
2024-02-29 16:26:41 +0200

    initial

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+class List{
+
+    public static List<int> DifferentElements(List<int> list1, List<int> list2){
+        HashSet<int> HashOne = new HashSet<int>(list1);
+        HashSet<int> HashTwo = new HashSet<int>(list2);
+
+        HashOne.SymmetricExceptWith(HashTwo);
+        int[] arrInt;
+        arrInt = HashOne.ToArray();
+        Array.Sort(arrInt);
+        HashOne.Clear();
+        HashOne.UnionWith(arrInt);
+
+        return new List<int>(HashOne);
+    }
+}
