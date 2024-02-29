fa6d76dd5d3bbe3181553b13c28821e13d349bb3
kwizerao <o.kwizera@alustudent.com>
2024-02-29 18:33:08 +0200

    initial

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+class LList{
+    public static LinkedList<int> CreatePrint(int size){
+
+        LinkedList<int> linkedList = new LinkedList<int>();
+
+        if (size < 0)
+        {
+            return linkedList;
+        }
+        else{
+            for(int i = 0; i < size; i++){
+                Console.WriteLine(i + "");
+                linkedList.AddFirst(i);
+            }
+        }
+        return linkedList;
+    }
+}
