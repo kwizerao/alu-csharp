be5a90540ab444863d58aa31e1ea3de9405d7203
kwizerao <o.kwizera@alustudent.com>
2024-02-29 18:39:28 +0200

    initial

---
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+class LList{
+
+    public static LinkedListNode<int>? Add(LinkedList<int> myLList, int n){
+        myLList.AddFirst(n);
+        return myLList.Find(n);
+    }
+}
