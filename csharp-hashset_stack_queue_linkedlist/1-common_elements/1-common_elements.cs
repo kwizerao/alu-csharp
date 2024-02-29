4831f06142e1915c5e394856229b6cb96d12b106
kwizerao <o.kwizera@alustudent.com>
2024-02-29 16:28:17 +0200

    initial

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+class List{
+
+    public static List<int> CommonElements(List<int> list1, List<int> list2){
+        HashSet<int> HashOne = new HashSet<int>(list1);
+        HashSet<int> HashTwo = new HashSet<int>(list2);
+
+        HashTwo.IntersectWith(HashOne);
+        List<int> nList = new List<int>(HashTwo);
+
+        return nList;
+    }
+}
