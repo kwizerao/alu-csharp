db7ebece6078a6e0eb41cf5d6efa1cff0e0f71bf
kwizerao <o.kwizera@alustudent.com>
2024-03-03 22:19:09 +0200

    initial

---
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System;
+
+class List{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength){
+        List<int> result = new List<int>();
+        int sonder = 0;
+//sonder will temporarily hold the result for the division
+        for(int i = 0; i < listLength ; i++){
+            try{
+                sonder = list1[i] / list2[i];
+                result.Add(sonder);
+            }catch(ArgumentOutOfRangeException){
+                Console.WriteLine("Out of range");
+            }catch(DivideByZeroException){
+                Console.WriteLine("Cannot divide by zero");
+                result.Add(0);
+            }
+        }
+
+        return result;
+    }
+}
