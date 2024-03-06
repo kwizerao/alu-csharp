975d4fa07a4acd9ad10692328569d31316ecf6bc
kwizerao <o.kwizera@alustudent.com>
2024-03-06 13:48:25 +0200

    initial

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -1,24 +1,21 @@
-﻿using System;
+﻿﻿using System;
 
-namespace Enemies{
-   
+namespace Enemies {
     /// <summary>
     /// Zombie is a public class 
     /// </summary>
-    public class Zombie{
+    public class Zombie {
 
         /// <summary>
         /// Holds the value of health
         /// </summary>
         public int health;
 
-
         /// <summary>
         /// Zombie Constructor inits health to zero
         /// </summary>
-        public Zombie(){
+        public Zombie() {
             health = 0;
         }
-
     }
 }
