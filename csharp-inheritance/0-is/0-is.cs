8c60d84d23c1b8d5bf1ad9601f98e3de925e7f33
kwizerao <o.kwizera@alustudent.com>
2024-03-14 12:28:15 +0200

    initial

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class Obj{
+
+/// <summary>
+/// Check for integer type
+/// </summary>
+/// <param name="obj"></param>
+/// <returns></returns>
+    public static bool IsOfTypeInt(Object obj){
+        if(obj is int){
+            return true;
+        }
+
+        return false;
+    }
+}
