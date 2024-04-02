24010c136649f7c4defab7782eead3570a8c40ea
kwizerao <o.kwizera@alustudent.com>
2024-04-02 11:57:36 +0200

    Initial

--- csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -1,20 +1,4 @@
 ﻿using System;
-/*
-class Program{
-    public static void Main(string[] args){
-        double[,] emtpy = {{}, { }};
-        double[,] result = MatrixMath.Transpose(emtpy);
-        for (int i = 0; i < result.GetLength(0); i++)
-        {
-            for (int j = 0; j < result.GetLength(1); j++)
-            {
-                Console.Write(result[i, j] + " ");
-            }
-            Console.WriteLine();
-        }
-    }
-}
-*/
 
 class MatrixMath{
     public static T[,] Transpose<T>(T[,] matrix){
